ef880fb64752219655d665077f9a431717a8d3e4
Oska <developer.oskang@gmail.com>
2019-12-22 00:29:24 +0800

    example: added enpoints intialize

--- example/service.go
+++ example/service.go
@@ -6,9 +6,8 @@ import (
 	"oscrud/action"
 	"oscrud/parser/basic"
 	ec "oscrud/transport/echo"
-	"oscrud/transport/http"
 
-	"github.com/labstack/echo"
+	"github.com/labstack/echo/v4"
 )
 
 // TestService :
@@ -21,48 +20,54 @@ func NewService() TestService {
 }
 
 // Find :
-func (t TestService) Find(service action.ServiceContext, ctx action.EndpointContext) error {
+func (t TestService) Find(service action.ServiceContext) error {
 	log.Println("You're accessing TestService.Find")
 	return nil
 }
 
 // Get :
-func (t TestService) Get(service action.ServiceContext, ctx action.EndpointContext) error {
+func (t TestService) Get(service action.ServiceContext) error {
 	log.Println("You're accessing TestService.Get")
 	return nil
 }
 
 // Create :
-func (t TestService) Create(service action.ServiceContext, ctx action.EndpointContext) error {
+func (t TestService) Create(service action.ServiceContext) error {
 	log.Println("You're accessing TestService.Create")
 	return nil
 }
 
 // Update :
-func (t TestService) Update(service action.ServiceContext, ctx action.EndpointContext) error {
+func (t TestService) Update(service action.ServiceContext) error {
 	log.Println("You're accessing TestService.Update")
 	return nil
 }
 
 // Patch :
-func (t TestService) Patch(service action.ServiceContext, ctx action.EndpointContext) error {
+func (t TestService) Patch(service action.ServiceContext) error {
 	log.Println("You're accessing TestService.Patch")
 	return nil
 }
 
 // Remove :
-func (t TestService) Remove(service action.ServiceContext, ctx action.EndpointContext) error {
+func (t TestService) Remove(service action.ServiceContext) error {
 	log.Println("You're accessing TestService.Remove")
+	return nil
+}
+
+// Test2 :
+func Test2(ctx action.EndpointContext) error {
+	log.Println("You're accessing Endpoint.")
 	return nil
 }
 
 func main() {
 	server := oscrud.NewOscrud()
 	server.RegisterService("test", NewService())
-	e := echo.New()
+	server.RegisterEndpoint("GET", "/test2", Test2)
+
 	server.RegisterTransport(
-		ec.NewEcho(e).UsePort(5001).UseParser(basic.NewParser()),
-		http.NewHTTP().UsePort(5000).UseParser(basic.NewParser()),
+		ec.NewEcho(echo.New()).UsePort(5001).UseParser(basic.NewParser()),
 	)
 	server.Start()
 }

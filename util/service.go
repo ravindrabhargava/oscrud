0557cacf3e7b4b8603c3c82cc1a9e20d7315af76
Oska <developer.oskang@gmail.com>
2019-12-25 02:46:55 +0800

    feat: internal purpose utility

---
+++ util/service.go
@@ -0,0 +1,39 @@
+package util
+
+import "strings"
+
+// TransformPath :
+func TransformPath(base, action string) string {
+	switch strings.ToLower(action) {
+	case "find":
+		return base + "/"
+	case "create":
+		return base + "/"
+	case "get":
+		return base + "/:id"
+	case "update":
+		return base + "/:id"
+	case "patch":
+		return base + "/:id"
+	case "remove":
+		return base + "/:id"
+	}
+	return ""
+}
+
+// GetMethodByAction :
+func GetMethodByAction(action string) string {
+	switch strings.ToLower(action) {
+	case "find", "get":
+		return "get"
+	case "create":
+		return "post"
+	case "update":
+		return "put"
+	case "patch":
+		return "patch"
+	case "remove":
+		return "delete"
+	}
+	return ""
+}

214f0d1560986c1e3ec5b6639883a951acae938e
Oskang09 <developer.oskang@gmail.com>
2020-06-08 00:48:45 +0800

    fix: remove get query

--- service.go
+++ service.go
@@ -25,7 +25,6 @@ type ServiceModel interface {
 	ToCreate() (interface{}, error)
 	ToResult() (interface{}, error)
 
-	ToGetQuery() (interface{}, error)
 	ToFindQuery() (interface{}, error)
 	ToPatchQuery() (interface{}, error)
 	ToUpdateQuery() (interface{}, error)

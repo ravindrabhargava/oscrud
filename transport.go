bc1446d00abcd02382c94b35dc50bdf3174d0938
meetravi007 <meetravi007@gmail.com>
2020-09-17 19:59:39 +0800

    uuid function added in transport.go

--- transport.go
+++ transport.go
@@ -13,3 +13,9 @@ type Transport interface {
 	Start() error
 	Name() TransportID
 }
+
+// function 'create_uuid' will generate the new uniqe id everytime whenever func will be call
+
+func create_uuid() {
+	fmt.Println(uuid.New().String())
+}
